a79e9980163b03f3341a17f9b75bd8afcdb89359
Damon Thomas <damon22t@gmail.com>
2024-11-19 10:59:03 -0500

    add db script and db queries

---
+++ model/populateDB.js
@@ -0,0 +1,79 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require('dotenv').config()
+
+const SQL = `
+CREATE TABLE all_inventory (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR ( 255 ), brand VARCHAR ( 255 ), battery BOOLEAN, price FLOAT, image VARCHAR (255));
+
+INSERT INTO all_inventory (name, brand, battery, price, image) 
+VALUES
+  ('Milwaukee Tool M18 18V Li-Ion Brushless Cordless Compact Hammer Drill/Impact Combo Kit (2-Tool) w/(2) 2.0 Batteries',
+   'Milwaukee',
+    'true', 
+    '198',
+    'https://images.homedepot.ca/productimages/p_1001797887.jpg?product-images=l'),
+    ('Milwaukee Tool 22-inch Packout Modular Tool Box Storage System',
+    'Milwaukee',
+    'false',
+    '298',
+    'https://images.homedepot.ca/productimages/p_1001076027.jpg?product-images=l'),
+    ('Milwaukee Tool M18 18V Lithium-Ion Brushless Cordless 7-1/4 -inch Circular Saw (Tool-Only)',
+    'Milwaukee',
+    'true',
+    '248',
+    'https://images.homedepot.ca/productimages/p_1001160058.jpg?product-images=l'),
+    ('DEWALT 20V MAX Atomic Cordless Brushless Compact Drill/Driver Kit with (1) 2Ah Battery',
+    'DEWALT',
+    'true',
+    '119',
+    'https://images.homedepot.ca/productimages/p_1001769719.jpg?product-images=l'),
+    ('MAKITA 12Vmax CXT Angle Drill w/Keyless Chuck (Tool Only)',
+    'MAKITA',
+    'true',
+    '109',
+    'https://images.homedepot.ca/productimages/p_1001311404.jpg?product-images=l'),
+    ('RYOBI 18V ONE+ Lithium-Ion Cordless Drill/Driver Kit with 1.5 Ah Battery and Charger',
+    'RYOBI',
+    'true',
+    '78',
+    'https://images.homedepot.ca/productimages/p_1001686659.jpg?product-images=l'),
+    ('DEWALT ATOMIC 25 ft. Tape Measure',
+    'DEWALT',
+    'false',
+    '9.98',
+    'https://images.homedepot.ca/productimages/p_1001663250.jpg?product-images=l'),
+    ('Milwaukee Tool FASTBACK Folding Utility Knife with Blade Storage and Compact Folding Utility Knife (2-Pack)',
+    'Milwaukee',
+    'false',
+    '19.98',
+    'https://images.homedepot.ca/productimages/p_1001584648.jpg?product-images=l'),
+    ('DEWALT 28oz Rip Claw Steel Framing Hammer',
+    'DEWALT', 
+    'false',
+    '29.98',
+    'https://images.homedepot.ca/productimages/p_1001221508.jpg?product-images=l'),
+    ('MAKITA 120 Pc. Accessory Kit',
+    'MAKITA',
+    'false',
+    '59.98',
+    'https://images.homedepot.ca/productimages/p_1001757490.jpg?product-images=l'),
+    ('RYOBI 18V ONE+ AirStrike 18-Gauge Cordless Lithium-Ion Brad Nailer (Tool-Only)',
+    'RYOBI',
+    'true',
+    '118', 
+    'https://images.homedepot.ca/productimages/p_1001627721.jpg?product-images=l');`;
+
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
--- model/query.js
+++ model/query.js
@@ -1,15 +1,47 @@
 const pool = require("./pool");
 
-async function getAllUsernames() {
-//   const { rows } = await pool.query("SELECT * FROM usernames");
-//   return rows;
+async function getAllTools() {
+  const { rows } = await pool.query("SELECT * FROM all_inventory");
+  console.log('all tools', rows)
+  return rows;
 }
 
-async function insertUsername(username) {
-//   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+async function insertProduct(name, brand, battery, price, image = 'assets/generaltool.png') {
+  await pool.query("INSERT INTO all_inventory (name, brand, battery, price, image) VALUES ($1, $2, $3, $4, $5)", [name, brand, battery, price, image]);
+}
+
+async function updateProduct(id, name, brand, battery, price, image = 'assets/generaltool.png') {
+  await pool.query(`UPDATE all_inventory SET name=${name}, brand=${brand}, battery=${battery}, price=${price}, image=${image} WHERE id=${id}`);
+}
+
+async function getDistinctBrands(brand) {
+  const { rows } = await pool.query("SELECT DISTINCT brand FROM all_inventory;")
+  console.log('distinct brands', rows)
+  return rows;
+}
+
+async function getToolsFromBrand(brand) {
+  const { rows } = await pool.query(`SELECT * FROM all_inventory WHERE brand = ${brand};`)
+  console.log('tools by brand', rows)
+  return rows;
+}
+
+async function toolsWithOrWOPower(bool) {
+  const { rows } = await pool.query(`SELECT * FROM all_inventory WHERE battery = ${bool};`)
+  console.log('tools by power', rows)
+  return rows;
+}
+
+async function deleteBrand(brand) {
+  await pool.query(`UPDATE all_inventory SET brand='other' WHERE brand=${brand}`);
 }
 
 module.exports = {
-  getAllUsernames,
-  insertUsername
+  getAllTools,
+  insertProduct,
+  getDistinctBrands,
+  getToolsFromBrand,
+  toolsWithOrWOPower,
+  updateProduct,
+  deleteBrand
 };
