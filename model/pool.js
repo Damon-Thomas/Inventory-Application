4f6a8406ea4d68dcae3f091c7edb3bce3dac2ee4
Damon Thomas <damon22t@gmail.com>
2024-11-30 16:19:53 -0500

    change env variable name

--- model/pool.js
+++ model/pool.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 
 
 module.exports = new Pool({
-  host: process.env.HOST, 
-  user: process.env.USER,
-  database: process.env.DB,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT
+  // host: process.env.HOST, 
+  // user: process.env.USER,
+  // database: process.env.DB,
+  // password: process.env.PASSWORD,
+  // port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL
 });
